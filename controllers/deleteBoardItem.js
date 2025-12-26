f3d1b4a9445fd64a80d7f8dc5baedb493f26df0d
Oliwier <oliwierrosiak07@gmail.com>
2025-12-26 23:01:27 +0100

    Added deleteing photos from aws while deleting board item from db

--- controllers/deleteBoardItem.js
+++ controllers/deleteBoardItem.js
@@ -1,4 +1,19 @@
 import { Notes } from "../db/dbConfig.js"
+import dotenv from 'dotenv'
+import s3Delete from "../s3/s3Delete.js"
+dotenv.config()
+
+async function deleteFromAWS(link)
+{
+    const key = link.split(`${process.env.AWS_ADDRESS}/`)[1]
+    try
+    {
+        await s3Delete(key)
+    }
+    catch(ex)
+    {
+    }
+}
 
 async function deleteBoardItem(req,res)
 {
@@ -15,11 +30,12 @@ async function deleteBoardItem(req,res)
         {
             throw new Error()
         }
-
+        if(elements[index].type === 'img' && elements[index].link.includes('https://interactive-board-storage.s3'))
+        {
+            deleteFromAWS(elements[index].link)
+        }
         elements.splice(index,1)
-
         note.content = elements
-
         await note.save()
 
         res.sendStatus(200)
---
+++ s3/s3Delete.js
@@ -0,0 +1,16 @@
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import s3 from "./s3Config.js";
+import dotenv from 'dotenv'
+dotenv.config()
+
+function s3Delete(key)
+{
+    const command = new DeleteObjectCommand({
+        Bucket:process.env.AWS_BUCKET_NAME,
+        Key:key
+    })
+
+    return s3.send(command)
+}
+
+export default s3Delete
